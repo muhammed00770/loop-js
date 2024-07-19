7f49c29636f004938e7af5322fca89ba32b7d9df
muhammed <mhd3252@gmail.com>
2024-07-19 09:49:08 +0530

    initial content

---
+++ for-04.js
@@ -0,0 +1,8 @@
+for(let i = 1;i <= 20; i++){
+    let str = '' ;
+for (let j = 1; j <= 1; j++){
+    str += i +" ";
+}
+    console.log(str);
+
+}    
---
+++ for-08.js
@@ -0,0 +1,8 @@
+for(let i = 1;i <= 45; i++){
+    let str = '' ;
+for (let j = 1; j <= 1; j++){
+    str += i +" ";
+}
+    console.log(str);
+
+}    
